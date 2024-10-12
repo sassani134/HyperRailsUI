a17847490490670c693fbd797db16d151696397b
Samuel <sassani134@gmail.com>
2024-10-12 16:43:59 +0200

    Add some test with it

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:blog) { FactoryBot.build(:blog) }
+
+  # Test of Blog validation
+  it "is valid with a title and content" do
+    blog = Blog.new(title: "title", content: "my content")
+    expect(blog).to be_valid
+  end
+
+  it "is invalid without a title" do
+    blog = Blog.new(title: nil, content: "My content")
+    expect(blog.errors[:title]).to include("can't be blank")
+  end
+
+  it "is invalid without content" do
+    blog = Blog.new(title: "My Title", content: nil)
+    expect(blog.errors[:content]).to include("can't be blank")
+  end
 end
